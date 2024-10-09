2c168d98a0db8cb40f8e91e885f36429b0b0406f
Le Quoc Bao <1209.quocbao@gmail.com>
2024-10-09 20:55:05 +0700

    Chỉnh sủa của Areas/Admin thêm các controller và view

---
+++ iStore/Areas/Admin/Controllers/MaGiamGiaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iStore.Areas.Admin.Controllers
+{
+    public class MaGiamGiaController : Controller
+    {
+        // GET: Admin/MaGiamGia
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
